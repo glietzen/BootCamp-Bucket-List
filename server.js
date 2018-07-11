93d6eae53678bbdb217628189354b8a9c9d9b618
Garrett Lietzen <glietzen@gmail.com>
2018-07-11 09:02:06 -0500

    gs

--- server.js
+++ server.js
@@ -35,12 +35,11 @@ app.use('/api/getUserInfo', getUserInfo);
 
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.use(express.static('../app/dist/')); // serve the static react app
+  app.get(/^\/(?!api).*/, (req, res) => { // don't serve api routes to react app
+    res.sendFile('index.html', { root: '../app/dist/' });
   });
+  console.log('Serving React App...');
 }
 
 
